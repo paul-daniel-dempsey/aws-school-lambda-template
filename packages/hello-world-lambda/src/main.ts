786324cdf851cbca69bd5ef507f8a49445f0fcb4
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-04-14 10:57:23 +0100

    first file load

--- packages/hello-world-lambda/src/main.ts
+++ packages/hello-world-lambda/src/main.ts
@@ -1,26 +1,30 @@
-import {Handler} from "aws-lambda";
-import {GetObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { Handler } from 'aws-lambda';
 
-const s3client = new S3Client({region: process.env["AWS_REGION"]});
+const s3client = new S3Client({ region: process.env['AWS_REGION'] });
 
-const handler:Handler = async (event, context) => {
-  const bucketName = process.env["BUCKET"];
-  const filePath = process.env["PATH"];
-  if(!bucketName || !filePath) {
-    throw new Error("You need to specify BUCKET and PATH in the environment configuration");
+const handler: Handler = async (event, context) => {
+  // console.log(event);
+  // console.log(context);
+  const bucketName = process.env['BUCKET'];
+  const filePath = process.env['PATH'];
+  if (!bucketName || !filePath) {
+    throw new Error(
+      'You need to specify BUCKET and PATH in the nevironment configuration'
+    );
   }
-
-  console.log(`Retrieving file at s3://${bucketName}/${filePath}...`);
+  console.log(`Retreiving file at s3://£{bucketName/${filePath}...`);
   const req = new GetObjectCommand({
     Bucket: bucketName,
     Key: filePath,
   });
   const response = await s3client.send(req);
-  console.log(`Found file with size ${response.ContentLength} and possibly checksum ${response.ChecksumSHA256}. Content was last modified at ${response.LastModified}`);
-  console.log("File content is: ");
+  console.log(
+    `Found file with size ${response.ContentLength} and possibly checksum ${response.ChecksumSHA256}.`
+  );
+  console.log('File contents is: ');
   const fileContent = await response.Body.transformToString();
   console.log(fileContent);
+};
 
-}
-
-export {handler}
+export { handler };
