aa2e8a6cca471e8ab22162561f950c664a583c41
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-05-14 14:05:28 +0100

    format tweaks

--- packages/hello-world-lambda/src/main.ts
+++ packages/hello-world-lambda/src/main.ts
@@ -1,28 +1,27 @@
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Handler } from 'aws-lambda';
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 const s3client = new S3Client({ region: process.env['AWS_REGION'] });
 
 const handler: Handler = async (event, context) => {
-  // console.log(event);
-  // console.log(context);
   const bucketName = process.env['BUCKET'];
   const filePath = process.env['PATH'];
   if (!bucketName || !filePath) {
     throw new Error(
-      'You need to specify BUCKET and PATH in the nevironment configuration'
+      'You need to specify BUCKET and PATH in the environment configuration'
     );
   }
-  console.log(`Retreiving file at s3://£{bucketName/${filePath}...`);
+
+  console.log(`Retrieving file at s3://${bucketName}/${filePath}...`);
   const req = new GetObjectCommand({
     Bucket: bucketName,
     Key: filePath,
   });
   const response = await s3client.send(req);
   console.log(
-    `Found file with size ${response.ContentLength} and possibly checksum ${response.ChecksumSHA256}.`
+    `Found file with size ${response.ContentLength} and possibly checksum ${response.ChecksumSHA256}. Content was last modified at ${response.LastModified}`
   );
-  console.log('File contents is: ');
+  console.log('File content is: ');
   const fileContent = await response.Body.transformToString();
   console.log(fileContent);
 };
