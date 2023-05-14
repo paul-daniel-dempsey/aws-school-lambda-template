981657bc35f61ea511162fdb8f92d9c58b6e7388
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-05-14 14:26:17 +0100

    sns message json parse shape

--- packages/hello-world-lambda/src/main.ts
+++ packages/hello-world-lambda/src/main.ts
@@ -1,11 +1,14 @@
 import { SNSEvent, SNSEventRecord, SNSHandler } from 'aws-lambda';
+import { EventShape } from './models';
 
 const handler: SNSHandler = (event: SNSEvent) => {
   console.log(`Received ${event.Records.length} events`);
   event.Records.forEach((evt: SNSEventRecord, ctr: number) => {
+    const content = JSON.parse(evt.Sns.Message) as EventShape;
     console.log(
-      `Message ${ctr}: ${evt.Sns.TopicArn} sebt ${evt.Sns.Message} at ${evt.Sns.Timestamp}`
+      `MessageSNS ${ctr}: ${evt.Sns.TopicArn} sent ${content.source} at ${evt.Sns.Timestamp}`
     );
+    console.log(`ContentSNS ${ctr}: ${content.content}`);
   });
 };
 
