ebed172d8a778649852b27d54bf304fa6c3b923b
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-05-14 15:50:09 +0100

    exampleIndexQuery

--- packages/message-receiver/src/dynamo.ts
+++ packages/message-receiver/src/dynamo.ts
@@ -1,7 +1,12 @@
-import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import {
+  AttributeValue,
+  DynamoDBClient,
+  PutItemCommand,
+  QueryCommand,
+} from '@aws-sdk/client-dynamodb';
 import { DynamoRecord, EventShape } from './models';
 import { createHash } from 'crypto';
-import { formatISO } from 'date-fns';
+import { formatISO, parseISO } from 'date-fns';
 
 const TableName = process.env['TABLE_NAME'];
 const ddbClient = new DynamoDBClient({ region: process.env["region'"] });
@@ -31,4 +36,39 @@ export async function DynamoRecordWrite(rec: DynamoRecord) {
     },
   });
   return ddbClient.send(req);
+}
+
+export async function exampleIndexQuery(
+  source: string,
+  characterCountLessThen: number
+): Promise<DynamoRecord[]> {
+  const req = new QueryCommand({
+    TableName,
+    IndexName: 'idxSourceCharcount',
+    KeyConditionExpression: 'Source = :s AND CharacterCount < :cc',
+    ExpressionAttributeValues: {
+      ':s': { S: source },
+      ':cc': { N: characterCountLessThen.toString() },
+    },
+  });
+  const response = await ddbClient.send(req);
+  console.log(`Returned a page of ${response.Count} items`);
+  const records = response.Items ? response.Items.map(MarshalDynamoRecord) : [];
+
+  records.forEach((rec, ctr) => {
+    console.log(`Item ${ctr}: ${JSON.stringify(rec)}  `);
+  });
+  return records;
+}
+
+export function MarshalDynamoRecord(
+  record: Record<string, AttributeValue>
+): DynamoRecord {
+  return {
+    characterCount: parseInt(record['CharacterCount'].N),
+    content: record['Content'].S,
+    contentHash: record['ContentHash'].S,
+    source: record['Source'].S,
+    timeStamp: parseISO(record['Timestamp'].S),
+  };
 }
