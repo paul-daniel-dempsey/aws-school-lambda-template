956b72aeead278d4f611edf40b81314bf534fa81
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-08-06 16:16:08 +0100

    session2

--- packages/hello-world-lambda/src/main.ts
+++ packages/hello-world-lambda/src/main.ts
@@ -1,7 +1,22 @@
-import {Handler} from "aws-lambda";
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { Handler } from 'aws-lambda';
 
-const handler:Handler = (event, context) => {
-  //Your code goes here!!
-}
+const handler: Handler = async (event, context) => {
+  const s3Client = new S3Client({ region: process.env['AW_REGION'] });
+  const bucketName = process.env['BUCKET'];
+  const filePath = process.env['PATH'];
+  if (!bucketName || !filePath) {
+    throw new Error('Specify BUCKET and PATH');
+  }
+  const req = new GetObjectCommand({
+    Bucket: bucketName,
+    Key: filePath,
+  });
+  const response = await s3Client.send(req);
+  console.log(`Found file with size ${response.ContentLength}...`);
+  console.log('File contents is: ');
+  const fileContent = await response.Body.transformToString();
+  console.log(fileContent);
+};
 
-export {handler}
+export { handler };
