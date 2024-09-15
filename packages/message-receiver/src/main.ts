9ce7dc644c25f0f8e19799edf1fb0fcca07ab137
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-09-15 20:54:44 +0100

    parsing function

--- packages/message-receiver/src/main.ts
+++ packages/message-receiver/src/main.ts
@@ -1,10 +1,18 @@
 import { SNSEvent, SNSEventRecord, SNSHandler } from 'aws-lambda';
 
+interface EventShape {
+  source?: string;
+  content: string;
+}
+
 export const handler: SNSHandler = (event: SNSEvent) => {
   console.log(`Received ${event.Records.length} events`);
   event.Records.forEach((evt: SNSEventRecord, ctr: number) => {
+    const content = JSON.parse(evt.Sns.Message) as EventShape;
+
     console.log(
-      `Message ${ctr}: ${evt.Sns.TopicArn} sent ${evt.Sns.Message} at ${evt.Sns.Timestamp}`
+      `Message ${ctr}: ${evt.Sns.TopicArn} sent ${content.source} at ${evt.Sns.Timestamp}`
     );
+    console.log(content.content);
   });
 };
