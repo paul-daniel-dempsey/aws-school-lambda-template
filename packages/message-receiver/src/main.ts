5ca001f0628daa263dd123c985b1cea3f4c86b58
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-09-15 20:50:09 +0100

    console log messages

--- packages/message-receiver/src/main.ts
+++ packages/message-receiver/src/main.ts
@@ -1,1 +1,10 @@
-console.log('Hello World');
+import { SNSEvent, SNSEventRecord, SNSHandler } from 'aws-lambda';
+
+export const handler: SNSHandler = (event: SNSEvent) => {
+  console.log(`Received ${event.Records.length} events`);
+  event.Records.forEach((evt: SNSEventRecord, ctr: number) => {
+    console.log(
+      `Message ${ctr}: ${evt.Sns.TopicArn} sent ${evt.Sns.Message} at ${evt.Sns.Timestamp}`
+    );
+  });
+};
